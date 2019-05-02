d8a66599cec5ab5e885f735ad4dde0a5ed4a22c5
Paul Solt <PaulSolt@gmail.com>
2019-05-02 13:29:55 -0400

    Added a stub implementation of the UICollectionViewController.

---
+++ Planets/PlanetCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PlanetCollectionViewCell.swift
+//  Planets
+//
+//  Created by Paul Solt on 5/2/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class PlanetCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var textLabel: UILabel!
+}
